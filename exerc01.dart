3da76cfcbd6d3aa0eba6217560cd4b47d2e23ae7
Igor Rauan <rauanigors@gmail.com>
2023-04-03 13:55:57 -0300

    exercicos adicionados

---
+++ exerc01.dart
@@ -0,0 +1,14 @@
+void main() {
+  int indice = 13;
+  int soma = 0;
+  int k = 0;
+
+  while (k < indice) {
+    k = k+1;
+    soma = soma + k;
+  }
+
+  print(soma);
+  print("o valor da soma será igual a $soma");
+
+}
